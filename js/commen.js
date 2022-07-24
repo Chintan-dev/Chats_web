05df0d1b713a364ee7d6858874e439d6466b8283
chintan-patel-developer <chintu20111999@gmail.com>
2022-07-24 22:49:59 +0530

    new change

--- js/commen.js
+++ js/commen.js
@@ -1,35 +1,35 @@
 
 
 $(function () {
-          $(".search_box").click(function () {
-                    $(".pop-box, .cover").show();
-                    // document.getElementById('pop-box').style.display = 'flex';
-                    // document.getElementById('cover').style.display = 'flex';
-          });
-          $(".cover, .x-btn").click(function () {
-                    $(".cover, .pop-box").hide();
-          });
+    $(".search_boxs").click(function () {
+        $(".pop-box, .cover").show();
+        // document.getElementById('pop-box').style.display = 'flex';
+        // document.getElementById('cover').style.display = 'flex';
+    });
+    $(".cover, .x-btn").click(function () {
+        $(".cover, .pop-box").hide();
+    });
 });
 
 
 function alertFunction() {
-          // alert('ok');
+    // alert('ok');
 }
 window.onload = alertFunction;
 
 
 // get user data
-search();
-function search() {
-          console.log("called: search");
-          var starCountRef = firebase.database().ref('Datas/users');
 
-          starCountRef.once('value', (snapshot) => {
-                    snapshot.forEach((childSnapshot) => {
-                              var childKey = childSnapshot.key;
-                              var childData = childSnapshot.val();
-                              if (childData.email !== firebase.auth().currentUser.email) {
-                                        data = `<div class="users_info">
+function search() {
+    console.log("called: search");
+    var starCountRef = firebase.database().ref('Datas/users');
+
+    starCountRef.once('value', (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+            if (childData.email !== firebase.auth().currentUser.email) {
+                data = `<div class="users_info">
                               <div class="box_s">
                                   <div class="img_s">
                                       <img src="${childData.photoURL}" id="photoURl" alt="">
@@ -42,18 +42,18 @@ function search() {
                                   <button>connect</button>
                               </div>
                           </div>`;
-                                        document.getElementById('get_data').innerHTML += data;
-                              }
-                    });
-          });
+                document.getElementById('get_data').innerHTML += data;
+            }
+        });
+    });
 }
-
+search();
 
 function msg_id_send_in_db(uid_f) {
-          const { displayName, photoURL, uid, email } = JSON.parse(localStorage.getItem('login_data'));
-          firebase.database().ref('Datas/connect/').push({
-                    uid: uid,
-                    uid_f: uid_f
-          });
-          alert("success");
+    const { displayName, photoURL, uid, email } = JSON.parse(localStorage.getItem('login_data'));
+    firebase.database().ref('Datas/connect/').push({
+        uid: uid,
+        uid_f: uid_f
+    });
+    alert("success");
 }
