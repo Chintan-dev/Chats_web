8be83b80beb55d689274a7ba808b534fe84365f3
chintan-patel-developer <chintu20111999@gmail.com>
2022-12-17 00:18:53 +0530

    update

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","") or die("Unble to connect");
-mysqli_select_db($conn ,'enjay_mini_project');
-
-?>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-           echo $no =$_REQUEST['fname'];
-          //echo $image = rand(1,5);
-?>
